c7a0dea5d7c2c852f176f95edff402e3f166f5c2
Tim <tim.w.lake@gmail.com>
2025-02-02 23:49:59 -0800

    init commit

---
+++ Source/TLake/TLake.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TLake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TLake, "TLake" );
