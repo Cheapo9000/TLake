695bf7cdc8417ade611449ec689995ff0ff3db86
Tim <tim.w.lake@gmail.com>
2025-11-11 01:45:29 -0800

    update to 5.5.4

---
+++ Source/TLake/TLakeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TLakeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATLakeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATLakeGameMode();
+};
+
+
+
